911769fc2941f5a1c32222cd4dc731fbba340da9
Kyle Johnson <joh04667@gmail.com>
2016-07-22 18:08:52 -0500

    werk

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
 var Appointment = Backbone.Model.extend({});
+var appointment = new Appointment();
+appointment.set('title', 'My knee hurts');
+var AppointmentView = Backbone.View.extend({
+render: function(){
+$(this.el).html('<li>' + this.model.get('title') + '</li>');
+}
+});
+var appointmentView = new AppointmentView({model: appointment});
 
-var appointment = new Appointment(
-  {title: 'thing'}
- );
-
-var AppointmentView = Backbone.View.extend({});
-
-var appointmentView = new AppointmentView({ model: appointment });
+appointmentView.render();
+$('#app').html(appointmentView.el);
